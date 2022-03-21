18f4da80f2d641b0dbdc23820700a7b1f994b474
Nicolae Alexandra <72298739+alexandranicolae@users.noreply.github.com>
2022-03-21 11:42:52 +0200

    Register Form

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -14,8 +14,12 @@ class DateFormular {
     parola1;
     parola2;
     newsletter;
-    termeni;
+    termeni = false;
 }
+
+const GIF_NAME = "imageonline-extractgif-65153-";
+const NO_OF_GIFS = 10;
+const GIFS_EXTENSION = ".gif";
 
 var inputDataPas1 = new Array();
 var inputDataPas2 = new Array();
@@ -61,15 +65,12 @@ btnSubmit.addEventListener('click', event => {
             $("body").append(gifImages[i]);
         }
 
-        $("body").append("<p>Multumim pentru completarea formularului, </p>")
-        //+dateForm.prenume+" "+dateForm.nume+"!");
-                .css("text-align", "center");
+        $("body").append("<p>Multumim pentru completarea formularului, "
+            +dateForm.prenume+" "+dateForm.nume+"!</p>")
+            .css("text-align", "center");
         $("body").append('<iframe src="https://giphy.com/embed/kddEqIIhp4soHjT6dP" width="480" height="320" frameBorder="0" class="giphy-embed"></iframe>');
         
     }
-    // else{
-    //     alert("Completeaza corect toate campurile obligatorii");
-    // }
 });
 
 for(let i=0; i<btnsBack.length; i++){
@@ -88,48 +89,48 @@ function verificaPas(pasCurent){
     switch(pasCurent){
         case 0 : 
         var ok = true;
-        // if(verificaInputRequiredEmpty(0)){
-        //     for(let i=0; i<inputDataPas1.length; i++){
-        //         if(!inputDataPas1[i]()){
-        //             ok = false;
-        //         }
-        //     }
-        // }
-        // else{
-            // ok = false;
-        //     alert("Completeaza toate campurile obligatorii");
-        // }
+        if(verificaInputRequiredEmpty(0)){
+            for(let i=0; i<inputDataPas1.length; i++){
+                if(!inputDataPas1[i]()){
+                    ok = false;
+                }
+            }
+        }
+        else{
+            ok = false;
+            alert("Completeaza toate campurile obligatorii");
+        }
         return ok;
         break;
 
         case 1 : 
         var ok = true;
-        // if(verificaInputRequiredEmpty(1)){
-        //     for(let i=0; i<inputDataPas3.length; i++){
-        //         if(!inputDataPas2[i]()){
-        //             ok = false;
-        //         }
-        //     }
-        // }
-        // else{
-        //    ok = false;
-        //     alert("Completeaza toate campurile obligatorii");
-        // }
+        if(verificaInputRequiredEmpty(1)){
+            for(let i=0; i<inputDataPas2.length; i++){
+                if(!inputDataPas2[i]()){
+                    ok = false;
+                }
+            }
+        }
+        else{
+           ok = false;
+            alert("Completeaza toate campurile obligatorii");
+        }
         return ok;
         break;
         case 2 : 
         var ok = true;
-        // if(verificaInputRequiredEmpty(2)){
-        //     for(let i=0; i<inputDataPas3.length; i++){
-        //         if(!inputDataPas3[i]()){
-        //             ok = false;
-        //         }
-        //     }
-        // }
-        // else{
-        //     ok = false;
-        //     alert("Completeaza toate campurile obligatorii");
-        // }
+        if(verificaInputRequiredEmpty(2)){
+            for(let i=0; i<inputDataPas3.length; i++){
+                if(!inputDataPas3[i]()){
+                    ok = false;
+                }
+            }
+        }
+        else{
+            ok = false;
+            alert("Completeaza toate campurile obligatorii");
+        }
         return ok;
         break;
         default : return true;
@@ -137,21 +138,24 @@ function verificaPas(pasCurent){
 }
 
 inputDataPas1.push(function adaugaImg(){
-    var img= document.getElementById("img");
+    var img = document.getElementById("img");
     dateForm.img=img.value; 
+    return true;
 });
 
 inputDataPas1.push(
     function verificaNume(){
-        var nume= document.getElementById("nume");
-        var aux= verificaDoarLitere(nume.value) && verificaLungimeMinim(nume.value,3);
+        var nume = document.getElementById("nume");
+        var aux = verificaDoarLitere(nume.value) && verificaLungimeMinim(nume.value,3);
         if(aux){
             dateForm.nume=nume.value; 
-            nume.classList.remove("validare"); 
+            nume.classList.remove("invalid"); 
         } 
         else{
-            $(nume).after("Acest nume nu este valid");
-            nume.classList.add("validare");  
+            $("#numeMsg").remove();
+            $(nume).after('<p id="numeMsg">Acest nume nu este valid</p>');
+            //$(nume).after("Acest nume nu este valid");
+            nume.classList.add("invalid");  
         }
         return aux; 
     }
@@ -163,11 +167,13 @@ inputDataPas1.push(
         var aux= verificaDoarLitere(prenume.value) && verificaLungimeMinim(prenume.value,3); 
         if(aux){
             dateForm.prenume=prenume.value; 
-            prenume.classList.remove("validare"); 
+            prenume.classList.remove("invalid"); 
         } 
         else{
-            $(prenume).after("Acest prenume nu este valid");
-            prenume.classList.add("validare");  
+            $("#prenumeMsg").remove();
+            $(prenume).after('<p id="prenumeMsg">Acest prenume nu este valid</p>');
+            //$(prenume).after("Acest prenume nu este valid");
+            prenume.classList.add("invalid");  
         }
         return aux;
     }
@@ -204,14 +210,17 @@ function verificaLungimeMaxim(input,maxim){
 
 inputDataPas1.push(
     function verificaSex(){
-        var radioBtns= document.getElementsByClassName("radio-btn");
-
-        if(getChecked(radioBtns)==-1){
-            cnp.classList.remove("validare");
+        var radioBtns = document.getElementsByClassName("radio-btn");
+        var sex = getChecked(radioBtns);
+        if( sex == -1 ){
+            cnp.classList.add("invalid");
+            return false;
+        }
+        else{
+            cnp.classList.remove("invalid");
+            dateForm.sex = (sex==1) ? "M" : "F";
             return true;
         }
-        cnp.classList.add("validare");
-        return false;
     }
 );
 
@@ -232,10 +241,9 @@ inputDataPas1.push(
         var dataNasterii= document.getElementById("datanasterii").value;
         
         // TODO verifica pe aici sa fie in parametri!
-        
-            dateForm.dataNasterii=dataNasterii;
-            cnp.classList.remove("validare");
-            return true;
+        dateForm.dataNasterii=dataNasterii;
+        cnp.classList.remove("invalid");
+        return true;
         
     }
 );
@@ -247,24 +255,25 @@ inputDataPas1.push(
         // X YY MM DD cod.... 
         // dim=13 , X= sex si data
         
-        let nr=1;
-        if(dateForm.sex=="F") nr=2;
+        let nr = 1;
+        if( dateForm.sex == "F") nr = 2;
 
         var data = new Date(dateForm.dataNasterii);
         let an = data.getFullYear().toString().substr(2,2);
         let luna = data.getMonth()+1;
         let zi = data.getDate();
 
-        if(cnp.length==13 && verificaDoarNr(cnp.value))
+        if(cnp.value.length==13 && verificaDoarNr(cnp.value))
             if(parseInt(cnp.value.substr(0,1))%2==nr%2)
             if(cnp.value.substr(1,2)==an && cnp.value.substr(3,2)==luna && cnp.value.substr(5,2)==zi)
             {   
-                cnp.classList.remove("validare");
+                cnp.classList.remove("invalid");
                 dateForm.cnp=cnp.value;
                 return true;
             }
-        $(cnp).after("Acest CNP nu este valid");
-        cnp.classList.add("validare");
+        $("#cnpMsg").remove();
+        $(cnp).after('<p id="cnpMsg">Acest CNP nu este valid</p>');
+        cnp.classList.add("invalid");
         return false;
     }
 );
@@ -279,8 +288,10 @@ inputDataPas2.push(
                 return true;
             }
         }
-        $(nrTel).after("Acest nr nu este valid. Incercati 07--------");
-        nrTel.classList.add("validare");
+        $("#nrTelMsg").remove();
+        $(nrTel).after('<p id="nrTelMsg">Acest nr nu este valid. Incercati 07--------</p>');
+        //$(nrTel).after("Acest nr nu este valid. Incercati 07--------");
+        nrTel.classList.add("invalid");
         return false;
     }
 );
@@ -291,33 +302,39 @@ inputDataPas2.push(
         
         var regex=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         if (email.value.match(regex)){
-            dateForm.email=email.value;
+            dateForm.email = email.value;
             return true;
         }
-        $(email).after("Acest mail nu este valid");
-        email.classList.add("validare");
+        $("#emailMsg").remove();
+        $(email).after('<p id="emailMsg">Acest mail nu este valid</p>');
+        //$(email).after("Acest mail nu este valid");
+        email.classList.add("invalid");
         return false;
     }
 )
 
 inputDataPas2.push(function adaugaJudet(){
-    var jud= document.getElementById("judete");
-    dateForm.jud=jud.value; 
+    var jud = document.getElementById("judete");
+    dateForm.jud = jud.value; 
+    return true;
 });
 
 inputDataPas2.push(function adaugaOras(){
-    var oras= document.getElementById("orase");
-    dateForm.oras=oras.value; 
+    var oras = document.getElementById("orase");
+    dateForm.oras = oras.value; 
+    return true;
 });
 
 inputDataPas2.push(function adaugaAdresa(){
-    var adresa = document.getElementById("adresa");
-    dateForm.adresa = adresa.value; 
+    var adresa = document.getElementById("adresa").value;
+    dateForm.adresa = adresa; 
+    return true;
 });
 
 inputDataPas2.push(function adaugaCodPostal(){
-    var cod = document.getElementById("codPostal");
-    dateForm.codPostal = cod.value; 
+    var codP = document.getElementById("codPostal").value;
+    dateForm.codPostal = codP; 
+    return true;
 });
 
 var parola1 = document.getElementById("parola");
@@ -325,18 +342,18 @@ var parola1 = document.getElementById("parola");
 inputDataPas3.push( verificaParola1 );
 function verificaParola1(){
     console.log(parola1.value);
-    var regex=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+    var regex=  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     if(parola1.value.match(regex)){
         dateForm.parola1=parola1.value;
 
-        parola1.classList.remove("validare");
+        parola1.classList.remove("invalid");
         parola1.classList.add("pass");
         return true;
     }
     else{
         parola1.classList.remove("pass");
         $("#parolaHintText").show();
-        parola1.classList.add("validare");
+        parola1.classList.add("invalid");
         return false;
     }
 }
@@ -350,13 +367,13 @@ function verificaParola2(){
         dateForm.parola2=parola2.value;
 
         parola2.classList.add("pass");
-        parola2.classList.remove("validare");
+        parola2.classList.remove("invalid");
         return true;
     }
     else{
         parola2.classList.remove("pass");
         $("#parola2HintText").show();
-        parola2.classList.add("validare");
+        parola2.classList.add("invalid");
         return false;
     }
 }
@@ -364,6 +381,7 @@ function verificaParola2(){
 inputDataPas3.push(function adaugaNewsletterOp(){
     var newsletter = document.getElementById("newsletter");
     dateForm.newsletter = newsletter.checked;
+    return true;
 });
 
 inputDataPas3.push(
@@ -371,7 +389,7 @@ inputDataPas3.push(
         var termeni= document.getElementById("termeni");
         
         if(termeni.checked){
-            dateForm.termeni = termeni;
+            dateForm.termeni = true;
             return true;
         }
         else{
@@ -389,11 +407,11 @@ for(let i=0; i < $(".required").length; i++){
     $(".required")[i].firstElementChild.innerText+="*";
 }
 
-function resetValidare(elemente){
-    for(let i=0; i < elemente.length; i++){
-        elemente[i].lastElementChild.classList.remove("validare");
-    }
-}
+// function resetValidare(elemente){
+//     for(let i=0; i < elemente.length; i++){
+//         elemente[i].lastElementChild.classList.remove("invalid");
+//     }
+// }
 
 function verificaInputRequiredEmpty(n){
     var ok = true;
@@ -403,10 +421,10 @@ function verificaInputRequiredEmpty(n){
         if(elemente[i].lastElementChild.tagName.toLowerCase() === 'input'){
             if(elemente[i].lastElementChild.value=="") {
                 ok = false;
-                elemente[i].lastElementChild.classList.add("validare");
+                elemente[i].lastElementChild.classList.add("invalid");
             }
             else{
-                elemente[i].lastElementChild.classList.remove("validare");
+                elemente[i].lastElementChild.classList.remove("invalid");
             }
         }
         else
@@ -420,14 +438,16 @@ function verificaInputRequiredEmpty(n){
     return ok;
 }
 
+
+/////////////// ====== GIF ====== \\\\\\\\\\\\\\\
 setInterval(displayGIF, 200);
 
 var count = 0;
 var gifImages = [];
 // adauga pozele
-for(let i = 0; i < 7; i++ ){
+for(let i = 0; i < NO_OF_GIFS; i++ ){
     var img = new Image();
-    img.src = "./Assets/Media/gifs/gif"+(i+1)+".png";
+    img.src = "./Assets/Media/gifs/"+GIF_NAME+(i+1)+GIFS_EXTENSION;
     img.classList.add("hidden");
     img.classList.add("img-gifs");
     gifImages.push(img);
@@ -445,6 +465,124 @@ function displayGIF() {
         gifImages[count+1].classList.remove("hidden"); //6
         count++;
     }
-    //displayNoneToggle(gifImages[count] , gifImages[count+1]);
 }
+
+
+//////////////// ==== GENERARE PAROLA =====\\\\\\\\\\\  
+
+// generare parola:
+var genereazaParola = (
+    length = 10,
+    wishlist = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$'
+    ) =>
+    Array.from(crypto.getRandomValues(new Uint32Array(length)))
+    .map((x) => wishlist[x % wishlist.length])
+    .join('')
+console.log(genereazaParola());
+
+
+
+// tooltip-ul
+let p = $('#parola');
+p.attr("title","Apasati tasta ↓(sageata-jos) pentru a genera o parola");
+console.log(p.title);
+$('#parola').tooltip({
+    trigger:"focus manual"
+});
+
+parola1.addEventListener("keyup", e =>{
+    if(e.key === "ArrowDown"){
+        parola1.value = genereazaParola();
+    }
+});
+
+/////////////========== EYE PASSWORD ========\\\\\\\\\\\
+
+$("#veziParola").mousedown(function () {
+    parola1.setAttribute("type","text");
+    
+    this.classList.remove("bi-eye");
+    this.classList.add("bi-eye-fill");
+});
+
+$("#veziParola").mouseup(function () {
+    parola1.setAttribute("type","password");
+    
+    this.classList.remove("bi-eye-fill");
+    this.classList.add("bi-eye");
+});
+
+$("#veziParola2").mousedown(function () {
+    parola2.setAttribute("type","text");
+    
+    this.classList.remove("bi-eye");
+    this.classList.add("bi-eye-fill");
+});
+
+$("#veziParola2").mouseup(function () {
+    parola2.setAttribute("type","password");
+    
+    this.classList.remove("bi-eye-fill");
+    this.classList.add("bi-eye");
+});
+
+
+
+
+////////// ===== INTERVIU TRIAL =====\\\\\\\\\\\
+
+// for(let i=1; i<=100; i++){
+// 	switch (true){
+//         case (i % 15 == 0) : console.log("FizzBuzz"); break;
+//         case (i % 3 == 0) : console.log("Fizz"); break;
+//         case (i % 5 == 0) : console.log("Buzz"); break;
+//         default: console.log(i);
+//     }
+// }
+
+// for(let i=1; i<=100; i++){
+//     if(i % 15 == 0)
+//         console.log("FizzBuzz");
+//     else{
+//         if(i % 5 == 0)
+//             console.log("Buzz");
+//         else{
+//             if(i % 3 == 0)
+//                 console.log("Fizz");
+//             else{
+//                 console.log(i);
+//             }
+//         }
+//     }
+// }
+
+
+// var v = [];
+// let i = 0;
+
+// function f(){
+//     if(i < 100){
+//         v.push(i + 1);
+//         console.log( v[i] );
+//         i ++;
+//         f();
+//     }
+//     else 
+//     console.log("done");
+// }
+
+// function f2(v){
+//     if(v.length == 100){
+//         return v;
+//     }
+//     v.push(v.length + 1);
+//     return f2(v);
+// }
+// console.log(f2([])); //// e mai ok sa facem fara param din afara
+
+
+// while(i < 100){
+//     v.push( i + 1 );
+//     i++;
+// } 
 
